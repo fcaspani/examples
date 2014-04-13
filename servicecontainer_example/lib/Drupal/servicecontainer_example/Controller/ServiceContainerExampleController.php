c07fcbac732890c97adf1caad18d0be1772d3e3d
Luca Lusso <llusso@wellnet.it>
2014-04-13 22:38:57 +0200

    aggiunti stub per eventdispatcher e servicecontainer

---
+++ servicecontainer_example/lib/Drupal/servicecontainer_example/Controller/ServiceContainerExampleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\servicecontainer_example\Controller;
+
+/**
+ * Class ServiceContainerExampleController
+ * @package Drupal\servicecontainer_example\Controller
+ */
+class ServiceContainerExampleController {
+
+  public function existingService() {
+    /** @var \Drupal\Core\Session\AccountInterface $currentUserService */
+    $currentUser = \Drupal::currentUser();
+
+    $build = array(
+      '#markup' => t('Logged username: @username',
+        array(
+          '@username' => $currentUser->getUsername(),
+        )
+      ),
+    );
+
+    return $build;
+  }
+
+} 
